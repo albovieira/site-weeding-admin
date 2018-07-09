90f560d2cb1ebce5483ff9ac6de90b3dde98f194
Albo Vieira <albovieira@gmail.com>
2018-07-08 22:36:52 -0300

    fix

--- src/config/settings/prod.js
+++ src/config/settings/prod.js
@@ -1,4 +1,4 @@
 export default {
   session_time: 7200000,
-  api: 'https://tannbimoib.execute-api.us-east-1.amazonaws.com/dev_cors/api/v1/'
+  api: 'https://nme56dww83.execute-api.us-east-1.amazonaws.com/dev/'
 };
